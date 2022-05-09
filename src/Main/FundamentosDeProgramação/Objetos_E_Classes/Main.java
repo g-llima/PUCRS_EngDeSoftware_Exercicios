27b1140f7db02e01fd3007fe05f77cdc7d48e7a6
Gabriel <gabrielrtxon@gmail.com>
2022-05-09 07:54:27 -0300

    Objects & Classes: Date class with getters, compare & clone methods

--- src/Main/FundamentosDeProgramação/Objetos_E_Classes/Main.java
+++ src/Main/FundamentosDeProgramação/Objetos_E_Classes/Main.java
@@ -4,6 +4,7 @@ import Main.FundamentosDeProgramação.Objetos_E_Classes.Ex4.Pessoa;
 import Main.FundamentosDeProgramação.Objetos_E_Classes.Ex5.ControleRemoto;
 import Main.FundamentosDeProgramação.Objetos_E_Classes.Ex5.Televisao;
 import Main.FundamentosDeProgramação.Objetos_E_Classes.Ex6.ContaCorrente;
+import Main.FundamentosDeProgramação.Objetos_E_Classes.Ex7.Data;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,14 +35,36 @@ public class Main {
 
 
 /*        ================| EXERCÍCIO 6 |=====================        */
-        ContaCorrente cc = new ContaCorrente(100);
-        System.out.println(cc.getSaldo());
 
-        cc.depositar(150);
-        System.out.println(cc.getSaldo());
+//        ContaCorrente cc = new ContaCorrente(100);
+//        System.out.println(cc.getSaldo());
+//
+//        cc.depositar(150);
+//        System.out.println(cc.getSaldo());
+//
+//        cc.sacar(200);
+//        System.out.println(cc.getSaldo());
 
-        cc.sacar(200);
-        System.out.println(cc.getSaldo());
 
+/*        ================| EXERCÍCIO 6 |=====================        */
+
+        Data data1 = new Data("06/01/2006");
+        Data data2 = new Data("07/08/2003");
+
+        System.out.println("==============| COMPARACAO |==============");
+        System.out.println(data1.compara(data2));
+
+        System.out.println("==============| METODOS DATA1 |==============");
+        System.out.println("Dia: " + data1.getDia());
+        System.out.println("Mes: " + data1.getMes());
+        System.out.println("Mes por extenso: " + data1.getMesExtenso());
+        System.out.println("Ano: " + data1.getAno());
+
+        System.out.println("==============| METODOS CLONE DA DATA1 |==============");
+        Data data3 = data1.clone();
+        System.out.println("Dia: " + data3.getDia());
+        System.out.println("Mes: " + data3.getMes());
+        System.out.println("Mes por extenso: " + data3.getMesExtenso());
+        System.out.println("Ano: " + data3.getAno());
     }
 }
