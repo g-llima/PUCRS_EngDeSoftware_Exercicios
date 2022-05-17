db87428ba0d362f80510ea3fee88478f10d8cd4e
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-05-16 22:28:14 -0300

    Arrays: Create two arrays, fill, print and sum them

--- src/Main/FundamentosDeProgramação/Vetores/Main.java
+++ src/Main/FundamentosDeProgramação/Vetores/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         //ex1();
-        ex2();
+        //ex2();
+        ex3();
     }
 
     static void ex1() {
@@ -67,5 +68,35 @@ public class Main {
         for (int n : vetor2) {
             System.out.print(n + " ");
         }
+    }
+    static void ex3() {
+        int[] vetor1 = new int[4];
+        int[] vetor2 = new int[4];
+        int soma = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vetor 1: ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Insira um valor: ");
+            vetor1[i] = sc.nextInt();
+        }
+
+        System.out.println("\nVetor 2: ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Insira um valor: ");
+            vetor2[i] = sc.nextInt();
+        }
+
+        for (int n : vetor1) {
+            System.out.print(n + " ");
+            soma += n;
+        }
+        System.out.println(" ");
+        for (int n : vetor2) {
+            System.out.print(n + " ");
+            soma += n;
+        }
+        System.out.println("\nSoma dos vetores: " + soma);
     }
 }
