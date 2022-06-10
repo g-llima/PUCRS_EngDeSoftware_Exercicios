f0cebbd9d76ff88f2344f7701e5b9a583cfd2c7b
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-10 18:33:45 -0300

    Trabalho final: método adicionar e pesquisar dinossauros

---
+++ src/Main/FundamentosDeProgramação/Trabalhos/TrabalhoFinal/Acoes/CadastroDinossauro.java
@@ -0,0 +1,35 @@
+package Main.FundamentosDeProgramação.Trabalhos.TrabalhoFinal.Acoes;
+
+import Main.FundamentosDeProgramação.Trabalhos.TrabalhoFinal.Entidades.Dinossauro;
+
+public class CadastroDinossauro {
+
+    Dinossauro[] dinossauros = new Dinossauro[100];
+
+    public boolean adicionarDinossauro(Dinossauro dino) {
+        int pos = -1;
+        for (int i = 0; i < dinossauros.length; i++)
+            if (dinossauros[i] == null) {
+                pos = i;
+                break;
+            }
+        if (pos != -1) {
+            dinossauros[pos] = dino;
+            return true;
+        }
+        return false;
+    }
+    public Dinossauro pesquisarDinossauro(int id) {
+        for (Dinossauro d : dinossauros) {
+            if (d == null) break;
+            if (d.getId() == id) return d;
+        }
+        return null;
+    }
+
+    public void exibirDinossauros() {
+        for (Dinossauro d : dinossauros) {
+            if (d != null) System.out.println(d);
+        }
+    }
+}
