df8b223b7f75da66dfb0efbea88f54951bbec46b
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-10 18:36:11 -0300

    Trabalho final - Atualizado main

--- src/Main/FundamentosDeProgramação/Trabalhos/TrabalhoFinal/Main.java
+++ src/Main/FundamentosDeProgramação/Trabalhos/TrabalhoFinal/Main.java
@@ -1,1 +1,26 @@
+package Main.FundamentosDeProgramação.Trabalhos.TrabalhoFinal;
 
+import Main.FundamentosDeProgramação.Trabalhos.TrabalhoFinal.Acoes.CadastroDinossauro;
+import Main.FundamentosDeProgramação.Trabalhos.TrabalhoFinal.Entidades.Dinossauro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CadastroDinossauro cd = new CadastroDinossauro();
+
+        Dinossauro d1 = new Dinossauro(0, "Raça do dino", 1, 203, 45);
+        Dinossauro d2 = new Dinossauro(1, "Raça do dino2", 1, 203, 45);
+        Dinossauro d3 = new Dinossauro(2, "Raça do dino2", 1, 203, 45);
+
+        System.out.println(cd.adicionarDinossauro(d1));
+        System.out.println(cd.adicionarDinossauro(d2));
+        System.out.println(cd.adicionarDinossauro(d3));
+
+        cd.exibirDinossauros();
+
+        //System.out.println(cd.removerDinossauro(0));
+
+        cd.exibirDinossauros();
+        //System.out.println(cd.pesquisarDinossauro(0));
+    }
+}
